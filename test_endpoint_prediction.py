b35c9980fc3bc867adf62a818d68fae9bab485d3
Kelvin Ting <kelvin@artoflogictech.com>
2024-10-26 13:13:42 +1030

    fix locatoin

--- test_endpoint_prediction.py
+++ test_endpoint_prediction.py
@@ -10,6 +10,8 @@ from delpoyment_config import *
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 if __name__ == "__main__":
+    aiplatform.init(project=PROJECT_ID, location=LOCATION)
+    
     logging.info("Making predictions from endpoint.")
     endpoint = aiplatform.Endpoint(str(END_POINT))
     test_data = np.load("data/X_test.npy")
